0597f49f31203068b1bdd1cc34eee42cf4823aed
shefinsajimon <165980801+shefinsajimon@users.noreply.github.com>
2024-04-05 11:36:05 +0530

    Update Program.vb

--- ConsoleApp11/Program.vb
+++ ConsoleApp11/Program.vb
@@ -1,19 +1,16 @@
 Module Program
     Sub Main(args As String())
-        ' Creating an instance of the Car class
         Dim myCar As New Car("Toyota", "Supra", 2018, "Blue")
 
-        ' Displaying car information
         Console.WriteLine("Car Details:")
         Console.WriteLine("Make: " & myCar.Make)
         Console.WriteLine("Model: " & myCar.Model)
         Console.WriteLine("Year: " & myCar.Year)
         Console.WriteLine("Color: " & myCar.Color)
 
-        ' Modifying car color
+
         myCar.ChangeColor("Black")
 
-        ' Displaying updated car information
         Console.WriteLine("Updated Color: " & myCar.Color)
 
         Console.ReadLine()
@@ -26,7 +23,6 @@ Public Class Car
     Public Property Year As Integer
     Public Property Color As String
 
-    ' Constructor to initialize car details
     Public Sub New(make As String, model As String, year As Integer, color As String)
         Me.Make = make
         Me.Model = model
@@ -34,7 +30,6 @@ Public Class Car
         Me.Color = color
     End Sub
 
-    ' Function to change car's color
     Public Sub ChangeColor(newColor As String)
         Me.Color = newColor
     End Sub
